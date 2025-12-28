d84b0e4ce5a2b0dd968609cb220ec3f70da1d2f2
mdsddm <md.saddam.gtm@gmail.com>
2025-12-29 00:05:05 +0530

    added express work

---
+++ ExpressJsDemo/index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const port = 8080;
+// This is for Routing 
+/*
+app.get("/", (req, res) => {
+    res.send("<h1>You Are in Root</h1>");
+});
+app.get("/home", (req, res) => {
+    res.send("<h1>You Are in home</h1>");
+});
+app.get("/contact", (req, res) => {
+    res.send("<h1>You Are in contact</h1>");
+});
+app.get("/apple", (req, res) => {
+    res.send("<h1>You Are in apple</h1>");
+});
+app.get("/banana", (req, res) => {
+    res.send("<h1>You Are in banana</h1>");
+});
+app.post("/", (req, res) => {
+    res.send("<h1>You Send Post Request</h1>");
+});
+
+// for any instead of below this will response
+app.use((req, res) => {
+    res.send("!!!!! No ! No ! You Are Smart Bro !!!!!");
+});
+
+*/
+// Now Come for Path parameter 
+app.get("/:username/:id", (req, res) => {
+    let { username, id } = req.params;
+    let str = `Welcome Mr. ${username} `;
+    res.send(str);
+});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
---
+++ importModuleDemo/scripts.js
@@ -0,0 +1,4 @@
+import { sum, PI } from "./math.js";
+console.log(sum(2, 3));
+console.log(PI);
+
