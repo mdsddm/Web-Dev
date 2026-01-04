91a01875a5926e92c29a9e972d37414d002bc54b
mdsddm <md.saddam.gtm@gmail.com>
2026-01-04 11:44:00 +0530

    completed rest_class

--- rest_class/index.js
+++ rest_class/index.js
@@ -3,9 +3,11 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
+
 const { v4: uuidv4 } = require('uuid');
+const methodOverride = require('method-override');
 
-
+app.use(methodOverride("_method"));
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
@@ -37,6 +39,25 @@ app.get("/posts/:id", (req, res) => {
     let post = posts.find((p) => id === p.id);
     res.render("show.ejs", { post });
 })
+// patch route
+app.patch("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p) => id === p.id);
+    post.content = newContent;
+    res.redirect("/posts")
+})
+app.get("/posts/:id/edit", (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("edit.ejs", { post });
+})
+app.delete("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    posts = posts.filter((p) => id !== p.id);
+    res.redirect("/posts");
+})
+
 
 app.listen(port, () => {
     console.log(`listening to ${port}`);
